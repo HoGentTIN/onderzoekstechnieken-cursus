0d8a58673d15c46196eefe6c4e8c8ac30c9e84d0
Wim Goedertier <wim.goedertier@hogent.be>
2018-02-24 01:43:21 +0100

    call biber if needed + added option --always

--- compile-all-tex.sh
+++ compile-all-tex.sh
@@ -3,23 +3,28 @@
 # grep -v -e '^$' -e '^#' .gitignore |sort -u
 
 pdflatex_cmd="pdflatex -interaction=batchmode -quiet"
-tmp_extensions="aux bib.bbl bcf bib.blg cmptexlog idx log nav out run.xml snm synctex.gz toc vrb"
-
-cmd=$0
-nothing_processed=1
+biber_cmd="biber --quiet"
+tmp_extensions="aux bcf bbl blg idx log nav out run.xml snm synctex.gz toc vrb"
 
 set -e
 set -u
 
+cmd=$0
+nothing_processed=1
+always=0
+
 usage() {
-    echo "Usage: $0 FILE [FILE [FILE ...]]"
-    echo "       $0 [DIRECTORY]"
-    #echo "Usage: $0 [-f|--force] FILE [FILE [FILE ...]]"
-    #echo "       $0 [-f|--force] [DIRECTORY]"
-    echo "Compile the Tex FILE(s) listed"
-    echo "Compile all Tex files in 'DIRECTORY' and its subdirs (default value: .)"
-    echo "By default, the files are compiled only if the Tex file is newer than the PDF"
-    #echo " -f, --force : *always* process the files, also if the TeX file is newer"
+    echo "Usage: $0 [-a] FILE [FILE [FILE ...]]"
+    echo "       $0 [-a] [DIRECTORY]"
+    echo "Compile the TeX FILE(s) listed"
+    echo "Compile all TeX files in 'DIRECTORY' and its subdirs (default value: .)"
+    echo "By default, the files are compiled only if the TeX file is newer than the PDF"
+    echo "By default, all temporary files are removed, but only after a successful build"
+    echo " -a, --always  : always process the files, also if the TeX file is newer"
+    #echo " -c, --cleanup : always remove tmp files, also if build failed or file is skipped"
+    #echo " -nc, --nocleanup: never remove temporary files, even if build was successful"
+    #echo " -b, --biber   : force to run biber (by default only if .bcf contains 'citekey')"
+    #echo " -nb, --nobiber: don't run biber"
 }
 
 die() {
@@ -39,17 +44,33 @@ compile_file() {
     cd $(dirname $1)
     filebase=$(basename $1)
     filebase=${filebase%.tex}
-    if [ -s ${filebase}.pdf -a ${filebase}.pdf -nt ${filebase}.tex ]; then
+    if [ $always -eq 0 -a -s ${filebase}.pdf -a ${filebase}.pdf -nt ${filebase}.tex ]; then
         echo "=== skipping $1 (PDF newer than TEX)"
         #ls -lrt ${filebase}.{tex,pdf} #for debugging
     else
-        exitcode=0
         echo "=== compiling $1"
+        for i in $tmp_extensions; do rm -f ${filebase}.$i; done
         rm -f ${filebase}.pdf
+        exitcode=0
         $pdflatex_cmd ${filebase}.tex || exitcode=$?
+        if [ $exitcode -eq 0 -a -f ${filebase}.bcf ]; then
+            if grep 'bcf:citekey' ${filebase}.bcf >/dev/null; then
+                echo "    running biber and recompiling"
+                exitcode=0
+                $biber_cmd $filebase || exitcode=$?
+                if [ $exitcode -eq 0 ]; then
+                    rm -f ${filebase}.pdf
+                    exitcode=0
+                    $pdflatex_cmd ${filebase}.tex || exitcode=$?
+                fi
+            fi
+        fi
         if [ $exitcode -eq 0 ]; then
             echo "    OK"
             # cleanup stuff
+            for i in $( grep '^\\\@input' ${filebase}.aux | tr '{' '}'|cut -d'}' -f2);
+                do rm -f $i;
+            done
             for i in $tmp_extensions; do rm -f ${filebase}.$i; done
             #ls -l ${filebase}.* #for debugging
         else
@@ -60,10 +81,31 @@ compile_file() {
     nothing_processed=0
 }
 
-if [ $# -eq 0 ]; then
-    search_files .
-fi
+## main ##
+
+for i in "$@"; do
+    case $i in
+    -a|--always) always=1 ;;
+    -*) die "Unexpected argument '$i'";;
+    esac
+done
+
+# get rid all options in beginning of command line
 while [ $# -gt 0 ]; do
+    case $1 in
+    -*) shift;;
+    *) break;;
+    esac
+done
+
+# if no "real" argument, search in current directory
+if [ $# -eq 0 ]; then search_files .; fi
+
+while [ $# -gt 0 ]; do
+    # get rid of options in between the arguments
+    case $1 in
+    -*) shift; continue;;
+    esac
     if [ -f $1 ]; then compile_file $1;
     elif [ -d $1 ]; then search_files $1;
     else
