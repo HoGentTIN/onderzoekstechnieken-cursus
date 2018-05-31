1c69084ba4d76fb723832e6380aa55e3b7d3d913
Wim Goedertier <wim.goedertier@hogent.be>
2018-05-31 09:33:37 +0200

    compile-all-tex: create *-vragen.pdf/*-oploss.pdf + several improvements

--- compile-all-tex.sh
+++ compile-all-tex.sh
@@ -14,7 +14,7 @@ fi
 biber_cmd="biber --quiet"
 
 # tmp_extensions: these files will be removed after a successful run
-tmp_extensions="aux bcf bbl blg idx log nav out run.xml snm synctex.gz toc vrb"
+tmp_extensions="aux,bcf,bbl,blg,idx,lof,log,lot,nav,out,run.xml,snm,synctex.gz,toc,vrb"
 
 # variables for argument parsing
 force=0
@@ -26,9 +26,11 @@ n_skipped=0
 n_failed=0
 n_succes=0
 
+tmpname=${0}.tmp
+
 usage() {
-    echo "Usage: $0 [-a] FILE [FILE [FILE ...]]"
-    echo "   or: $0 [-a] [DIR [DIR [DIR ...]]]"
+    echo "Usage: $0 [-f] [-c|-nc] FILE [FILE [FILE ...]]"
+    echo "   or: $0 [-f] [-c|-nc] [DIR [DIR [DIR ...]]]"
     echo "Compile the specified TeX FILE(s) or compile *all* TeX files"
     echo "in the specified DIR(s) and all the underlying subdirs (default value: .)"
     echo "By default, the files are compiled only if the TeX file is newer than the PDF"
@@ -39,6 +41,7 @@ usage() {
     echo " -h, --help      : show this help message"
     #TODO echo " -b, --biber   : force to run biber (by default only if .bcf contains 'citekey')"
     #TODO echo " -nb, --nobiber: don't run biber"
+    echo "Send questions, requests, issues, bugs, ... to wim.goedertier@hogent.be"
 }
 
 die() {
@@ -54,90 +57,120 @@ search_files() {
         process_file $i
     done
 }
+check_needrebuild() {
+    # $1 is a filename (without extension) to be checked
+    # return 0 (true) if rebuild is needed
+    # return 1 (false) if rebuild is not needed
 
+    if grep '^\\solution' ${filebase}.tex >/dev/null; then
+        [ -s ${filebase}-vragen.pdf ] || return 0
+        [ ${filebase}-vragen.pdf -nt ${filebase}.tex ] || return 0
+        for tmpfile in $(grep '\\include{' ${filebase}.tex|cut -b10-|tr -d '}'); do
+            [ ${filebase}-vragen.pdf -nt ${tmpfile}.tex ] || return 0
+        done
+        [ -s ${filebase}-oploss.pdf ] || return 0
+        [ ${filebase}-oploss.pdf -nt ${filebase}.tex ] || return 0
+        for tmpfile in $(grep '\\include{' ${filebase}.tex|cut -b10-|tr -d '}'); do
+            [ ${filebase}-oploss.pdf -nt ${tmpfile}.tex ] || return 0
+        done
+    else
+        [ -s ${filebase}.pdf ] || return 0
+        [ ${filebase}.pdf -nt ${filebase}.tex ] || return 0
+        for tmpfile in $(grep '\\include{' ${filebase}.tex|cut -b10-|tr -d '}'); do
+            [ ${filebase}.pdf -nt ${tmpfile}.tex ] || return 0
+        done
+    fi
+    return 1
+}
 process_file() {
     #echo process_file $1 #debug
-    cd $(dirname $1)
+    foldername=$(dirname $1)
+    cd $foldername
     filebase=$(basename $1)
     filebase=${filebase%.tex}
-    # checking if (one of) the tex file(s) has changed
-    needrebuild=1
-    if [ -s ${filebase}.pdf ]; then
-        if [ ${filebase}.pdf -nt ${filebase}.tex ]; then
-            needrebuild=0
-            for tmpfile in $(grep '\\include{' ${filebase}.tex|cut -b10-|tr -d '}'); do
-                if [ ${filebase}.pdf -ot ${tmpfile}.tex ]; then
-                    needrebuild=1
-                    break
-                fi
-            done
-        fi
-    fi
+
+    if check_needrebuild $filebase; then needrebuild=1
+    else needrebuild=0; fi
+
     if [ $force -eq 0 -a $needrebuild -eq 0 ]; then
         n_skipped=$((n_skipped + 1))
-        echo "=== skipping $1"
-        if [ $cleanup -eq 1 ]; then cleanup $filebase; fi
+        echo "=== skipping ${foldername}/${filebase}.tex"
+        if [ $cleanup -eq 1 ]; then
+            if grep '^\\solution' ${filebase}.tex >/dev/null; then
+                cleanup ${filebase}-vragen
+                cleanup ${filebase}-oploss
+            else
+                cleanup $filebase
+            fi
+        fi
     else
         if grep '^\\solution' ${filebase}.tex >/dev/null; then
-            sed 's/^\\solution.*/\\solutiontrue/' ${filebase}.tex >${filebase}-opl.tex
-            compile_file ${filebase}-opl
-            rm ${filebase}-opl.tex
+            sed 's/^\\solution.*/\\solutionfalse/' ${filebase}.tex >$tmpname
+            compile_file $foldername $tmpname ${filebase}-vragen
+            sed 's/^\\solution.*/\\solutiontrue/' ${filebase}.tex >$tmpname
+            compile_file $foldername $tmpname ${filebase}-oploss
+            rm $tmpname
+        else
+            compile_file $foldername ${filebase}.tex ${filebase}
         fi
-        compile_file ${filebase}
     fi
     cd - >/dev/null
 }
 
 compile_file() {
-    filebase2=$1
-    echo "=== compiling $filebase2"
+    foldername=$1
+    inputname=$2
+    outputname=$3
+    echo "=== creating ${foldername}/${outputname}.pdf"
     # remove old tmp files from previous run
-    for i in $tmp_extensions; do rm -f ${filebase2}.$i; done
+    #eval rm -f ${outputname}.{$tmp_extensions}
     exitcode=0
-    rm -f ${filebase2}.pdf ${filebase2}-withERRORS.pdf || exitcode=$?
+    rm -f ${outputname}.pdf ${outputname}-withERRORS.pdf || exitcode=$?
     # compile, only if we were able to remove the previous pdf
     if [ $exitcode -eq 0 ]; then
-        $pdflatex_cmd ${filebase2}.tex || exitcode=$?
+        $pdflatex_cmd -jobname=$outputname $inputname || exitcode=$?
+        compilecount=1
     fi
     # run biber, only if necessary
-    if [ $exitcode -eq 0 -a -f ${filebase2}.bcf ]; then
-        if grep 'bcf:citekey' ${filebase2}.bcf >/dev/null; then
-            echo "    running biber and recompiling"
-            $biber_cmd $filebase2 || exitcode=$?
+    if [ $exitcode -eq 0 -a -f ${outputname}.bcf ]; then
+        if grep 'bcf:citekey' ${outputname}.bcf >/dev/null; then
+            echo "    running biber and recompiling (pass 2)"
+            $biber_cmd $outputname || exitcode=$?
             # compile 2nd time, only if biber was successful
             if [ $exitcode -eq 0 ]; then
-                $pdflatex_cmd ${filebase2}.tex || exitcode=$?
+                $pdflatex_cmd -jobname=$outputname $inputname || exitcode=$?
+                compilecount=2
             else
                 echo "    if biber failed due to UTF-8 errors, it might help to run following command:"
                 echo "    iconv -f WINDOWS-1252 -t UTF-8 biblio.bib >tmpfile; mv tmpfile biblio.bib"
             fi
         fi
-    elif [ $exitcode -eq 0 ]; then
-        # compile 2nd time, if necessary
-        changed=1
-        grep 'Package rerunfilecheck Info: File' -A1 ${filebase2}.log | tr -d '\n' | grep -e 'has not changed' >/dev/null && changed=0
-        #TODO: add double check
-        #grep 'Package rerunfilecheck Warning: File' -A1 ${filebase2}.log | tr -d '\n' | grep -e 'has changed' && changed=1
-        if [ $changed -eq 1 ]; then
-            echo "    compiling 2nd time"
-            $pdflatex_cmd ${filebase2}.tex || exitcode=$?
-        fi
     fi
+    while [ $exitcode -eq 0 ]; do
+        # check if need to rerun
+        if grep 'Rerun to' ${outputname}.log >/dev/null; then
+            compilecount=$((compilecount + 1))
+            echo "    compiling again (pass ${compilecount})"
+            $pdflatex_cmd -jobname=$outputname $inputname || exitcode=$?
+        else
+            break
+        fi
+    done
     if [ $exitcode -eq 0 ]; then
         n_succes=$((n_succes + 1))
         echo "    OK"
-        if [ $nocleanup -eq 0 ]; then cleanup $filebase2; fi
+        if [ $nocleanup -eq 0 ]; then cleanup $outputname; fi
     else
         n_failed=$((n_failed + 1))
         # errors from pdflatex doesn't always end with a newline
         # so, the 'echo' below will add a newline
-        echo 
+        echo " !! Build FAILED !!"
         # rename pdf if build failed
-        if [ -f ${filebase2}.pdf ]; then
-            mv ${filebase2}.pdf ${filebase2}-withERRORS.pdf || tmp=0 #don't die on failure
+        if [ -f ${outputname}.pdf ]; then
+            mv ${outputname}.pdf ${outputname}-withERRORS.pdf || tmp=0 #don't die on failure
         fi
         # cleanup if requested
-        if [ $cleanup -eq 1 ]; then cleanup $filebase2; fi
+        if [ $cleanup -eq 1 ]; then cleanup $outputname; fi
     fi
 }
 
@@ -149,7 +182,7 @@ cleanup() {
             do rm -f $i;
         done
     fi
-    for i in $tmp_extensions; do rm -f ${1}.$i; done
+    eval rm -f ${1}.{$tmp_extensions}
 }
 
 ## main ##
@@ -163,6 +196,8 @@ for i in "$@"; do
     -*) die "Unexpected argument '$i'";;
     esac
 done
+
+echo "=== type 'sh $0 --help' for more details"
 
 # get rid of all options in beginning of command line
 while [ $# -gt 0 ]; do
@@ -180,7 +215,16 @@ while [ $# -gt 0 ]; do
     case $1 in
     -*) shift; continue;;
     esac
-    if [ -f $1 ]; then process_file $1;
+    if [ -f $1 ]; then
+        if echo $1 | grep '\.tex$' >/dev/null; then
+            if grep '^\\documentclass' $1 >/dev/null; then
+                process_file $1
+            else
+                echo "=== skipping $1 (\\documentclass is missing) ==="
+            fi
+        else
+            echo "=== skipping $1 (not a .tex file) ==="
+        fi
     elif [ -d $1 ]; then search_files $1;
     else
         echo "=== skipping $1 (not a FILE or DIRECTORY) ==="
@@ -189,12 +233,13 @@ while [ $# -gt 0 ]; do
 done
 
 n_total=$((n_skipped + n_failed + n_succes))
+echo
+echo "= REPORT ="
 if [ $n_total -eq 0 ]; then
-    die 'no valid TeX files found'
+    echo "  There were no valid TeX files found"
+    echo "  Type 'sh $0 --help' for more details"
 else
-    echo
-    echo "= REPORT ="
-    [ $n_skipped -eq 0 ] || echo "  $n_skipped/$n_total files were skipped because PDF was newer than TeX file ==="
-    [ $n_succes -eq 0 ] || echo "  $n_succes/$n_total TeX files successfully compiled into PDF file ==="
-    [ $n_failed -eq 0 ] || echo "  $n_failed/$n_total ERRORS: TeX files were NOT compiled successfully ==="
+    [ $n_skipped -eq 0 ] || echo "  $n_skipped/$n_total files were skipped because PDF was newer than TeX file"
+    [ $n_succes -eq 0 ] || echo "  $n_succes/$n_total TeX files successfully compiled into PDF file"
+    [ $n_failed -eq 0 ] || echo "  $n_failed/$n_total ERRORS: TeX files were NOT compiled successfully"
 fi
